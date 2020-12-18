2a1190f27ec88cc450687e482ce6fa0cde398544
Logan Hemphill <logan.hemphill86@gmail.com>
2020-12-18 16:19:29 -0500

    Created New logo, newGame btn, test pg2 and styling for all

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,19 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import landing from "../images/landing.jpg"
+import ohHell from "../images/ohHell.jpg"
+import newGame from "../images/newGame.jpg"
 import "../styling/index.css"
+import GatsbyImage from "gatsby-image"
 
 const IndexPage = () => (
   <div id="main">
-
-    </div>
+    <img id="ohimg" src={ohHell} alt="Oh Hell Logo" />
+    <Link id="ngLink" to="/page-2">
+      <img id="ngImg" src={newGame} alt="New Game" />
+    </Link>
+    {/* <img id="ngimg" src={newGame} alt="New Game" /> */}
+  </div>
 )
 
 export default IndexPage
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,14 +3,13 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import icon from "../images/gatsby-icon.png"
+import "../styling/pg2.css"
 
 const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  <div id="main">
+    <img id="icon" src={icon} alt="Gatsby Icon" />
+  </div>
 )
 
 export default SecondPage
